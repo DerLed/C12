b862544c4282f78255fc4ef9efa2f8a40061059d
DerLed <lediburit@mail.ru>
2021-12-19 20:42:44 +0300

    more i

---
+++ C12Frame.py
@@ -0,0 +1,75 @@
+import tkinter as tk
+from IT14_tolerance import IT14_TOLERANCE as IT14
+
+t_c12 = 0
+
+
+class C12Frame(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.lbl_D1 = tk.Label(self, text="Наружный диаметр")
+        self.lbl_D2 = tk.Label(self, text="Внутрений диаметр")
+
+        self.entry_D1 = tk.Entry(self)
+        self.entry_D2 = tk.Entry(self)
+
+        self.btn_calc = tk.Button(self, text="Вычислить", command=self.calc)
+        self.btn_to_clipboard = tk.Button(self, text="Скопировать в буфер", command=self.to_clipboard)
+
+        self.result = tk.Label(self, text="", height=6, justify=tk.LEFT)
+
+        self.entry_D1.bind('<Return>', lambda event: self.entry_D2.focus())
+        self.entry_D1.bind('<KP_Enter>', lambda event: self.entry_D2.focus())
+        self.entry_D2.bind('<Return>', lambda event: self.calc())
+        self.entry_D2.bind('<KP_Enter>', lambda event: self.calc())
+
+        self.lbl_D1.grid(column=0, row=0)
+        self.lbl_D2.grid(column=0, row=1)
+        self.entry_D1.grid(column=1, row=0)
+        self.entry_D2.grid(column=1, row=1)
+        self.btn_calc.grid(column=0, row=2)
+        self.btn_to_clipboard.grid(column=1, row=2)
+        self.result.grid(column=0, columnspan=2)
+        self.entry_D1.focus()
+
+    def calc(self):
+        global t_c12
+        if self.entry_D1.get():
+            out_d = float(self.entry_D1.get())
+        else:
+            self.result["text"] = "Вы не ввели одно или несколько значений"
+            return
+        if self.entry_D2.get():
+            inner_d = float(self.entry_D2.get())
+        else:
+            self.result["text"] = "Вы не ввели одно или несколько значений"
+            return
+        tolerance_out_d = 0
+        tolerance_inner_d = 0
+        for o_d in IT14[::-1]:
+            if out_d >= o_d[0]:
+                tolerance_out_d = o_d[1] / 1000
+                break
+
+        for in_d in IT14[::-1]:
+            if inner_d >= in_d[0]:
+                tolerance_inner_d = in_d[1] / 1000
+                break
+        t_c12 = round((tolerance_out_d + tolerance_inner_d)/2, 3)
+        print(tolerance_out_d, tolerance_inner_d, t_c12)
+        self.result["text"] = f'Наружный диаметр: Da = {out_d} мм\n' \
+                              f'Допуск на наружный диаметр: {tolerance_out_d} мм\n' \
+                              f'Внутренний диаметр: Da = {inner_d} мм\n' \
+                              f'Допуск на внутренний диаметр: {tolerance_inner_d} мм\n' \
+                              f'Толщина стенки: S = {(out_d-inner_d)/2} мм\n' \
+                              f'Технологическая прибавка: С = {t_c12} мм'
+
+    def to_clipboard(self) -> None:
+        if t_c12:
+            print(t_c12)
+        r = tk.Tk()
+        r.withdraw()
+        r.clipboard_clear()
+        r.clipboard_append(t_c12)
+        r.destroy()
--- main.py
+++ main.py
@@ -1,77 +1,67 @@
 import tkinter as tk
+from C12Frame import C12Frame
 
 
-class IT14:
-    def __init__(self):
-        self.H14_list = [
-            (1, 250),
-            (3, 300),
-            (6, 360),
-            (10, 430),
-            (18, 520),
-            (30, 620),
-            (50, 740),
-            (80, 870),
-            (120, 1000),
-            (180, 1150),
-            (250, 1300),
-            (315, 1400),
-            (400, 1550)
-        ]
+# class IT14:
+#     def __init__(self):
+#         self.H14_list = [
+#             (1, 250),
+#             (3, 300),
+#             (6, 360),
+#             (10, 430),
+#             (18, 520),
+#             (30, 620),
+#             (50, 740),
+#             (80, 870),
+#             (120, 1000),
+#             (180, 1150),
+#             (250, 1300),
+#             (315, 1400),
+#             (400, 1550)
+#         ]
 
-        self.h14_list = [
-            (1, -250),
-            (3, -300),
-            (6, -360),
-            (10, -430),
-            (18, -520),
-            (30, -620),
-            (50, -740),
-            (80, -870),
-            (120, -1000),
-            (180, -1150),
-            (250, -1300),
-            (315, -1400),
-            (400, -1550)
-        ]
+# self.h14_list = [
+#     (1, -250),
+#     (3, -300),
+#     (6, -360),
+#     (10, -430),
+#     (18, -520),
+#     (30, -620),
+#     (50, -740),
+#     (80, -870),
+#     (120, -1000),
+#     (180, -1150),
+#     (250, -1300),
+#     (315, -1400),
+#     (400, -1550),
+#     (500, -1750),
+#     (630, -2000),
+#     (800, -2300),
+#     (1000, -2600),
+#     (1250, -3100),
+#     (1600, -3700),
+#     (2000, -4400),
+#     (2500, -5400)
 
-        print(self.h14_list[::-1])
+# ]
 
-    def get_value(self, out_d: int, inner_d: int) -> tuple:
-        pr_out_d = 0
-        pr_inner_d = 0
+# print(self.h14_list[::-1])
 
-        for o_d in self.h14_list[::-1]:
-            if out_d >= o_d[0]:
-                pr_out_d = abs(o_d[1]) / 1000
-                break
+# def get_value(self, out_d: int, inner_d: int) -> tuple:
+#     pr_out_d = 0
+#     pr_inner_d = 0
 
-        for in_d in self.H14_list[::-1]:
-            if inner_d >= in_d[0]:
-                pr_inner_d = in_d[1] / 1000
-                break
-        t_c12 = round((pr_out_d + pr_inner_d)/2, 3)
-        return pr_out_d, pr_inner_d, t_c12
+# for o_d in self.h14_list[::-1]:
+#     if out_d >= o_d[0]:
+#         pr_out_d = abs(o_d[1]) / 1000
+#         break
 
-
-class C12Frame(tk.Frame):
-    def __init__(self, master):
-        super().__init__(master)
-
-        self.lbl_D1 = tk.Label(self, text="Наружный диаметр")
-        self.lbl_D2 = tk.Label(self, text="Внутрений диаметр")
-        self.entry_D1 = tk.Entry(self)
-        self.entry_D2 = tk.Entry(self)
-        self.btn_calc = tk.Button(self, text="Вычислить", command=self.calc)
-
-        self.lbl_D1.grid(column=0, row=0)
-        self.lbl_D2.grid(column=0, row=1)
-        self.entry_D1.grid(column=1, row=0)
-        self.entry_D2.grid(column=1, row=1)
-        self.btn_calc.grid(column=0)
-
-    def calc(self):
-        print(self.entry_D1.get())
+# for in_d in self.H14_list[::-1]:
+#     if inner_d >= in_d[0]:
+#         pr_inner_d = in_d[1] / 1000
+#         break
+# t_c12 = round((pr_out_d + pr_inner_d)/2, 3)
+# return pr_out_d, pr_inner_d, t_c12
 
 
 class App(tk.Tk):
@@ -83,7 +73,7 @@ class App(tk.Tk):
 
 
 if __name__ == "__main__":
-    d = IT14()
-    print(d.get_value(159, 142))
+
+
     app = App()
     app.mainloop()
